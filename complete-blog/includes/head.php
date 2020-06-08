26d99dccf16e22fe9f9dffd35563b5916eab85bd
Amipalsson-cme <65658341+Amipalsson-cme@users.noreply.github.com>
2020-06-08 19:57:54 +0200

    Update head.php

--- complete-blog/includes/head.php
+++ complete-blog/includes/head.php
@@ -6,6 +6,6 @@
 <meta http-equiv="X-UA-Compatible" content ="ie=edge">
 <title>My Blog </title>
 
-<link rel ="stylesheet" href="../css/bootstrap.min.css">
-<link rel ="stylesheet" href="../css/bootstrap.css">
+<link rel ="stylesheet" href="css/bootstrap.min.css">
+<link rel ="stylesheet" href="css/bootstrap.css">
 <link rel ="stylesheet" href="css/style.css">
